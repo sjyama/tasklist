1b186225151605c00f3b6e1f4dacdc22f9099c60
sj <grinpeace101@gmail.com>
2017-05-08 07:26:02 +0000

    kadai-tasklist

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,56 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+    #@task = Task.new(content: 'input-task')
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:success] = 'タスク追加：正常終了'
+      redirect_to @task
+      #redirect_to tasks_url
+    else
+      flash.now[:danger] = 'タスク追加：異常終了'
+      render :new
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      flash[:success] = 'タスク更新：正常終了'
+      redirect_to @task
+      #redirect_to tasks_url
+    else
+      flash.now[:danger] = 'タスク更新：異常終了'
+      render :edit
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    flash[:success] = 'タスク削除：正常終了'
+    redirect_to tasks_url
+  end
+end
+
+private
+
+#Strong Parameter
+def task_params
+  params.require(:task).permit(:content)
+end
